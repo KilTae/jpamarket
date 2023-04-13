5645950b094d25805347ee2cc3c6d79baaf5eaa4
Kiltae <loverpleasestayo0@gmail.com>
2023-04-13 19:52:02 +0900

    FEAT controller

---
+++ src/main/java/jpabook/jpamarket/Controller/MemberForm.java
@@ -0,0 +1,16 @@
+package jpabook.jpamarket.Controller;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Service
+public class MemberForm {
+
+    @NotEmpty(message = "회원 이름은 필수")
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+}
