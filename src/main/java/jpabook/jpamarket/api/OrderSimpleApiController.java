b2d1882098ecc354924f1ceb2c768b119202557c
Kiltae <loverpleasestayo0@gmail.com>
2023-05-03 16:24:07 +0900

    FEAT order api

---
+++ src/main/java/jpabook/jpamarket/api/OrderSimpleApiController.java
@@ -0,0 +1,46 @@
+package jpabook.jpamarket.api;
+
+
+import jpabook.jpamarket.Address;
+import jpabook.jpamarket.Order;
+import jpabook.jpamarket.repository.OrderRepository;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            address = order.getDelivery().getAddress();
+        }
+
+    }
+}
